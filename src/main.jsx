d0fc23e80e993d8fee3575152780b402f631c0e9
HasibX2000 <hasiburbd24@gmail.com>
2024-04-13 23:39:25 +0600

    cdn fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./app.css";
 import { Provider } from "react-redux";
-import store from './redux/store.jsx'
+import store from "./redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
